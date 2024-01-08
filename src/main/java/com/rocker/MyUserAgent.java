b169f439a03b343c327d248f4941a31699232031
srisailam-kurva <srisailam.kurva@cloudtern.com>
2024-01-08 19:54:12 +0530

    added rocker fw app

---
+++ src/main/java/com/rocker/MyUserAgent.java
@@ -0,0 +1,24 @@
+package com.rocker;
+
+import org.xhtmlrenderer.pdf.ITextOutputDevice;
+import org.xhtmlrenderer.pdf.ITextUserAgent;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class MyUserAgent extends ITextUserAgent {
+    @Override
+    protected InputStream resolveAndOpenStream(String url) {
+        try {
+            URL resolvedUrl = new URL(url);
+            return resolvedUrl.openStream();
+        } catch (IOException e) {
+            throw new RuntimeException("external css path invalid :-"+e);
+        }
+    }
+
+    public MyUserAgent(ITextOutputDevice outputDevice) {
+        super(outputDevice);
+    }
+}
